46aa9e539661eb1ff9e71fad1eb64d000603db80
raffaelecapaldo <raffaelecpl@gmail.com>
2023-04-20 16:20:02 +0200

    Added Youtube component

--- src/components/Trailers.vue
+++ src/components/Trailers.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="youtube">
+    <div class="youtube d-flex justify-content-center">
         <!-- A causa delle politiche di chrome, un video in autoplay parte solo se mutato, rimuovo il mute appena l'utente interagisce con la pagina -->
-        <iframe width="1900" height="1200" :src="videoToPlay"
-            frameborder="0"></iframe>
+        <YouTube class="player" :src="videoToPlay" :vars="{
+            autoplay: 1,
+            mute: 1,
+        }" @ready="onReady" width="1900" height="1200" ref="youtube" />
+        <font-awesome-icon @click="toggleVolume()" class="icon"
+            :icon="!muted ? 'fa-solid fa-volume-high' : 'fa-solid fa-volume-xmark'" /> />
     </div>
 </template>
 
@@ -14,22 +18,28 @@ export default {
     methods: {
         getRndInteger(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        toggleVolume() {
+            this.$refs.youtube.isMuted() ? this.$refs.youtube.unMute() : this.$refs.youtube.mute();
+            this.muted = !this.muted
+
         }
     },
     computed: {
         videoToPlay() {
             return this.videos[this.getRndInteger(0, this.videos.length)];//Prendi un video a caso tra quelli nell'array
-        }
+        },
     },
     data() {
         return {
             store,
             videos: [
-                'https://www.youtube-nocookie.com/embed/3CQ0HsGZzM4?controls=0&autoplay=1&mute=1&',
-                'https://www.youtube-nocookie.com/embed/eyOP-gA4tIo?controls=0&autoplay=1&mute=1&',
-                'https://www.youtube-nocookie.com/embed/o7nkJDjuSp4?controls=0&autoplay=1&mute=1&',
-                'https://www.youtube-nocookie.com/embed/8-UEA3SLKQI?controls=0&autoplay=1&mute=1&',
-            ]
+                'https://www.youtube-nocookie.com/embed/3CQ0HsGZzM4',
+                'https://www.youtube-nocookie.com/embed/eyOP-gA4tIo',
+                'https://www.youtube-nocookie.com/embed/o7nkJDjuSp4',
+                'https://www.youtube-nocookie.com/embed/8-UEA3SLKQI',
+            ],
+            muted: true,
         }
     }
 
@@ -48,9 +58,20 @@ export default {
 
 }
 
-iframe {
+.player {
     position: absolute;
     top: -200px;
     height: 1200px;
+}
+
+.icon {
+    position: absolute;
+    top: 20px;
+    font-size: 5rem;
+    background-color: white;
+    border-radius: 50%;
+    padding: 20px;
+    right: 50px;
+    cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import YouTube from 'vue3-youtube'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-library.add(faStar)
+import { faStar, faVolumeXmark, faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar, faVolumeXmark, faVolumeHigh)
 
 
 
 import './assets/styles/general.scss'
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).component('YouTube', YouTube).mount('#app')
